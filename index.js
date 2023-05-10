d569d1be3713133a04c351728118666d79be3c5d
mostafizurrahman <dev.mostafizurrahman@gmail.com>
2023-05-10 10:04:26 +0600

    class 7 added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require ('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app =express()
 const port =process.env.PORT || 7000;
 
@@ -45,6 +45,23 @@ async function run() {
       res.send(result)
       
     })
+
+    app.delete('/coffee/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result=await coffeeCollection.deleteOne(query)
+      res.send(result)
+
+    })
+
+    app.get('/coffee/:id', async(req,res)=>{
+      const id=req.params.id;
+      const query={_id :new ObjectId(id)}
+      const result=await coffeeCollection.findOne(query)
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
